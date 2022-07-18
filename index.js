f4fc8a811c29888e12e0a88df19b38adc3d69fe9
RJAttwell <richardjattwell@gmail.com>
2022-07-18 13:33:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//Interact with user's local file system using node
+
+const fs = require('fs');      
+//const stands for constant and you cannot change the value it's assigned, unlike variable.
+//const is only available in ES6
+
+//fs stands for file system. Can name the const whatever but for simplicity, I will use fs
