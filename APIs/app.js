d7741f62ff713390e3e93a6999ca19c8043a74f5
RJAttwell <richardjattwell@gmail.com>
2023-01-27 17:20:20 +0000

    Add files via upload

--- APIs/app.js
+++ APIs/app.js
@@ -9,13 +9,40 @@ const app = express();
 
 app.get("/", function(req, res){
 
-    const url = "";
-    //Use a callback function
-    https.get(url, function(){
-        console.log(response);
-    });
+    //Must have the https://
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Dublin&lat=44.34&lon=10.99&units=metric&appid=9c9edcc138322b7cc7be2b3e360dc3fe";
+   
 
-    res.send("Server is up and running")
+    //Use a callback function
+    https.get(url, function(response){
+        //Can log the status code associated with the response. Should hopefully be 200
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            //Will provide hexidecimal data
+            //console.log(data)
+
+            //Convert data into Javascript object
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+
+            //Challenge to print the weather description
+            const weatherDescription = weatherData.weather[0].description
+            res.send("The temperature in London is " + temp + " degrees Celcius.");
+            //Can only have one res.send in any given app method
+
+
+
+            //Can also convert Javascript object into a string
+            // const object = {
+            //     name: "Richard",
+            //     favouriteFood: "General Tso's Chicken"
+            // };
+
+            //console.log(JSON.stringify(object));
+        })
+    });
+    
 })
 
 
